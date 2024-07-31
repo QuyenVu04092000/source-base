0110995d1a6074ac7cc8a1ea5c0a041656be1e13
Quyen <vuquyen.04092000@gmail.com>
2024-08-01 05:25:51 +0700

    set up bottom sheet and modal

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, Platform, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function ModalScreen() {
+  const sheetRef = React.useRef<BottomSheet>(null);
+  const [sheetIndex, setSheetIndex] = React.useState(-1); // Initial state to hide the sheet
+  const snapPoints = ['100%'];
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
@@ -13,6 +18,21 @@ export default function ModalScreen() {
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Button title="Open Bottom Sheet" onPress={() => setSheetIndex(0)} />
+          <BottomSheet
+            ref={sheetRef}
+            index={sheetIndex}
+            snapPoints={snapPoints}
+            onChange={index => setSheetIndex(index)}
+          >
+            <View style={{ flex: 1, alignItems: 'center' }}>
+              <Text>Awesome 🎉</Text>
+            </View>
+          </BottomSheet>
+        </View>
+      </GestureHandlerRootView>
     </View>
   );
 }
